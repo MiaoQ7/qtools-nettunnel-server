74dbe15f63b63282dbf9d6d1edee85ef8ac6f700
chenqi <1311949161@qq.com>
2023-12-21 11:51:45 +0800

    init

---
+++ app.js
@@ -0,0 +1,228 @@
+
+
+
+
+
+
+const http = require('http');
+const url = require('url');
+const querystring = require('querystring');
+const fs = require('fs');
+const path = require('path');
+const net = require('net');
+const {processToCheck,processToKill,startProcess,checkNginx,reloadNginx,createSSL} = require('./checkProcess')
+
+//===========nginx config=========================
+const nginx_conf_path = '/www/server/panel/vhost/nginx'
+const nginx_root_path = '/www/wwwroot'
+const bt_panel_path = '/www/server/panel'
+const nginx_bin_path = '/www/server/nginx/sbin/nginx'
+//================nginx config end=================
+
+//===================frps config==========================
+const frps_path = '/opt/frps'
+//===================frps config end==========================
+
+async function startFrps() {
+  if (await processToCheck('frps')) {
+    await processToKill('frps')
+  }
+
+  startProcess(frps_path + '/frps', ['-c', 'frps.toml'], frps_path)
+}
+
+startFrps()
+
+// /www/server/panel/pyenv/bin/python /www/server/panel/class/acme_v2.py --domain a.qq.miaomiao.press --type http --path /www/wwwroot/a.qq.miaomiao.press
+// nginx -t
+// nginx -s reload
+
+async function restartNginx() {
+  if (await checkNginx(nginx_bin_path)) {
+    await reloadNginx(nginx_bin_path)
+  }
+}
+
+function deleteDirectory(directoryPath) {
+  // 递归删除目录及其内容
+  if (fs.existsSync(directoryPath)) {
+    fs.readdirSync(directoryPath).forEach(file => {
+      const filePath = path.join(directoryPath, file);
+
+      if (fs.lstatSync(filePath).isDirectory()) {
+        // 如果是子目录，则递归删除子目录
+        deleteDirectory(filePath);
+      } else {
+        // 如果是文件，则删除文件
+        fs.unlinkSync(filePath);
+      }
+    });
+
+    // 删除空目录
+    fs.rmdirSync(directoryPath);
+    console.log('Directory deleted:', directoryPath);
+  } else {
+    console.error('Directory does not exist:', directoryPath);
+  }
+}
+
+function deleteFilesInDirectory(directoryPath) {
+  // 读取目录中的所有文件
+  return new Promise(r => {
+    fs.readdir(directoryPath, (err, files) => {
+      if (err) {
+        console.error('Error reading directory:', err);
+        r()
+        return;
+      }
+      let p = []
+      files.forEach(file => {
+        const filePath = path.join(directoryPath, file);
+        if (filePath.indexOf('.qq.miaomiao.press') >= 0) {
+          if (fs.lstatSync(filePath).isDirectory()) {
+            deleteDirectory(filePath)
+          } else {
+            p.push(new Promise(rr => {
+              fs.unlink(filePath, err => {
+                if (err) {
+                  console.error('Error deleting file:', err);
+                } else {
+                  console.log('Deleted file:', filePath);
+                }
+                rr()
+              })
+            }))
+          }
+        }
+      });
+
+      Promise.all(p).then(r)
+    });
+  })
+}
+
+// 每24小时执行一次
+setInterval(async () => {
+  console.log('run delete all')
+  // 删除所有配置
+  await deleteFilesInDirectory(nginx_conf_path)
+  await deleteFilesInDirectory(nginx_root_path)
+  // 重启nginx
+  await restartNginx()
+  // 重启frps
+  await startFrps()
+}, 1000 * 3600)
+
+function generateRandomString(length) {
+  const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+  let randomString = '';
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * alphabet.length);
+    randomString += alphabet.charAt(randomIndex);
+  }
+
+  return randomString;
+}
+
+const randomInt = (min, max) => Math.floor(Math.random() * (max - min) + min);
+
+function isPortTaken(port) {
+  return new Promise((resolve) => {
+    const tester = net.createServer()
+      .once('error', (err) => {
+        if (err.code === 'EADDRINUSE') {
+          resolve(true);
+        } else {
+          resolve(false);
+        }
+      })
+      .once('listening', () => {
+        tester.once('close', () => {
+          resolve(false);
+        }).close();
+      })
+      .listen(port);
+  });
+}
+
+async function findDomainAndPort() {
+  let count = 100
+  while (count > 0) {
+    count--
+    let domain = generateRandomString(6)
+    if (fs.existsSync(path.join(nginx_conf_path, domain + '.qq.miaomiao.press.conf'))) {
+      continue
+    }
+    let port = randomInt(50000, 60000)
+    const isPortInUse = await isPortTaken(port);
+    if (isPortInUse) {
+      continue
+    }
+    return {
+      domain: domain + '.qq.miaomiao.press',
+      port: port //  50000-60000
+    }
+  }
+  return {domain: '', port: 0}
+}
+
+async function createNginx({domain, port}) {
+  let buf = fs.readFileSync(path.join(__dirname, 'template.conf'), { encoding: 'utf-8' })
+  buf = buf.replace(/\$\{server_name\}/g, domain)
+  let confPath = path.join(nginx_conf_path, domain + '.conf')
+  if (fs.existsSync(confPath)) {
+    throw Error('domain has exist')
+  }
+  fs.writeFileSync(confPath, buf, { encoding: 'utf-8' })
+  fs.mkdirSync(path.join(nginx_root_path, domain))
+  await restartNginx()
+  await createSSL(`${bt_panel_path}/pyenv/bin/python ${bt_panel_path}/class/acme_v2.py --domain ${doamin} --type http --path ${nginx_root_path}/${domain}`)
+
+  buf = fs.readFileSync(path.join(__dirname, 'template_proxy.conf'), { encoding: 'utf-8' })
+  buf = buf.replace(/\$\{server_name\}/g, domain).replace('${proxy_address}', `http://127.0.0.1:${port}`)
+  confPath = path.join(nginx_conf_path, domain + '.conf')
+  if (fs.existsSync(confPath)) {
+    fs.unlinkSync(confPath)
+  }
+  fs.writeFileSync(confPath, buf, { encoding: 'utf-8' })
+  fs.mkdirSync(path.join(nginx_root_path, domain))
+  await restartNginx()
+}
+
+const server = http.createServer(async (req, res) => {
+  try {
+    // 解析请求的 URL
+    const parsedUrl = url.parse(req.url);
+    // 解析查询参数
+    const queryParams = querystring.parse(parsedUrl.query);
+    if (parsedUrl == '/add') {
+      let conf = await findDomainAndPort()
+      if (!conf.domain) {
+        throw Error('domain not find')
+      }
+      await createNginx(conf)
+    }
+
+    // 设置响应头
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+
+    // 发送响应数据
+    res.end(`success`);
+  } catch (e) {
+    console.log(e)
+    // 设置响应头
+    res.writeHead(500, {'Content-Type': 'text/plain'});
+
+    // 发送响应数据
+    res.end(`response error`);
+  }
+});
+
+const port = 3000;
+const host = '127.0.0.1';
+
+server.listen(port, host, () => {
+  console.log(`Server is running at http://${host}:${port}/`);
+});
+
---
+++ checkProcess.js
@@ -0,0 +1,105 @@
+
+
+const { exec, spawn } = require('child_process');
+
+module.exports = {
+  processToCheck (name) {
+    return new Promise((resolve, reject) => {
+      exec(`ps aux | grep '[${name[0]}]${name.slice(1)}'`, (error, stdout, stderr) => {
+        if (error) {
+          console.error(`Error checking for ${name}: ${error.message}`);
+          resolve(false)
+          return;
+        }
+
+        if (stdout.includes(name)) {
+          console.log(`${name} is running.`);
+          resolve(true)
+        } else {
+          console.log(`${name} is not running.`);
+          resolve(false)
+        }
+      });
+    })
+  },
+  processToKill (name) {
+    return new Promise((resolve, reject) => {
+      exec(`pkill ${name}`, (error, stdout, stderr) => {
+        if (error) {
+          console.error(`Error killing ${name}: ${error.message}`);
+          reject(error.message)
+          return;
+        }
+
+        if (stderr) {
+          console.error(`Error output from killing ${name}: ${stderr}`);
+          resolve(false)
+        } else {
+          console.log(`${name} process has been terminated.`);
+          resolve(true)
+        }
+      });
+    })
+  },
+  startProcess(name, args, cwd) {
+    const frpsProcess = spawn(name, args, {
+      detached: true,
+      stdio: 'ignore',
+      cwd: cwd,
+    });
+    frpsProcess.unref();
+
+    console.log('frps process is running in the background.');
+  },
+  checkNginx(nginxPath) {
+    return new Promise((resolve, reject) => {
+      exec(nginxPath + ' -t', (error, stdout, stderr) => {
+        if (error) {
+          console.error('Error running nginx -t:', error.message);
+          console.error('stderr:', stderr);
+          resolve(false)
+        } else {
+          // 检查命令是否正常退出
+          if (stderr.includes('successful')) {
+            console.log('nginx -t is successful. Nginx configuration is valid.');
+            resolve(true)
+          } else {
+            console.error('nginx -t returned an error. Nginx configuration is invalid.');
+            console.error('stderr:', stderr);
+            resolve(false)
+          }
+        }
+      });
+    })
+  },
+  reloadNginx(nginxPath) {
+    return new Promise((resolve, reject) => {
+      exec(nginxPath + ' -s reload', (error, stdout, stderr) => {
+        if (error) {
+          console.error('Error running nginx -s reload:', error.message);
+          console.error('stderr:', stderr);
+          // 在这里可以进行错误处理逻辑
+          resolve(false)
+        } else {
+          console.log('Nginx reloaded successfully.');
+          console.log('stdout:', stdout);
+          // 在这里可以进行成功处理逻辑
+          resolve(true)
+        }
+      });
+    })
+  },
+  createSSL(cmd) {
+    return new Promise((resolve, reject) => {
+      exec(cmd, (error, stdout, stderr) => {
+        if (error) {
+          // 在这里可以进行错误处理逻辑
+          resolve(false)
+        } else {
+          // 在这里可以进行成功处理逻辑
+          resolve(true)
+        }
+      });
+    })
+  }
+ }
